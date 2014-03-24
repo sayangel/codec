0384478acf88b53e95802fe5f0062acb018d418f
sayangel <sayangel@gmail.com>
2014-03-23 23:20:35 -0400

    reviving codec

--- background.js
+++ background.js
@@ -2,7 +2,25 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details) { 
   	console.log("ENTERED:")
   	console.log(details.url);
-  	chrome.windows.create({url:"http://www.codecademy.com"})
+  	chrome.windows.getAll({"populate":true}, function(windows) {
+	    var existing_tab = null;
+	    for (var i in windows) {
+	      var tabs = windows[i].tabs;
+	      for (var j in tabs) {
+	        var tab = tabs[j];
+	        if (tab.url.indexOf('learn.html')!= -1) {
+	          existing_tab = tab;
+	          break;
+	        }
+	      }
+	    }
+	    if (existing_tab) {
+	      chrome.tabs.update(existing_tab.id, {"selected":true});
+	    } else {
+	      chrome.tabs.create({"url":"learn.html", "selected":true});
+	    }
+    });
+  	//chrome.windows.create({url:"http://www.codecademy.com"})
   	//return {cancel: true};
   },
   {
